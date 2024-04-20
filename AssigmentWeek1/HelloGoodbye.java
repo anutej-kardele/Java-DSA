917fe7c4aec867b174440b2372b49efe52089768
Anutej Kardele <anutej.kardele@gmail.com>
2024-04-20 10:40:03 +0530

    assigment week 1

--- AssigmentWeek1/HelloGoodbye.java
+++ AssigmentWeek1/HelloGoodbye.java
@@ -2,9 +2,8 @@ package AssigmentWeek1;
 
 public class HelloGoodbye {
     public static void main(String[] args){
-        String a = args[0];
-        String b = args[1];
-        System.out.println("Hello " + a + " and " + b);
-        System.out.println("Goodbye " + b + " and " + a);
+        
+        System.out.println("Hello " + args[0] + " and " + args[1]);
+        System.out.println("Goodbye " + args[1] + " and " + args[0]);
     }
 }
--- AssigmentWeek1/RandomWord.java
+++ AssigmentWeek1/RandomWord.java
@@ -1,21 +1,32 @@
 package AssigmentWeek1;
+//import edu.princeton.cs.algs4.StdIn;
+//import edu.princeton.cs.algs4.StdOut;
+//import edu.princeton.cs.algs4.StdRandom;
 
 public class RandomWord {
-    public static void main(String[] args){
-
-        shuffle(args);
-        // Generate random integers in range 0 to 999
-        System.out.println(args[0]);
-
-    }
-    public static void shuffle(Object[] a) {
-        int n = a.length;
-        for (int i = 0; i < n; i++) {
-            // choose index uniformly in [0, i]
-            int r = (int) (Math.random() * (i + 1));
-            Object swap = a[r];
-            a[r] = a[i];
-            a[i] = swap;
-        }
+    public static void main(String[] args) {
+//        int index = 0;
+//        String champion = "";
+//
+//        while (!StdIn.isEmpty()) {
+//          String word = StdIn.readString();
+//          boolean accept = StdRandom.bernoulli(1 / (index + 1.0));
+//          if (accept) {
+//            champion = word;
+//          }
+//          index++;
+//        }
+//        StdOut.println(champion);
     }
 }
+
+// int n = args.length;
+//         for (int i = 0; i < n; i++) {
+//             // choose index uniformly in [0, i]
+//             int r = (int) (Math.random() * (i + 1));
+//             String swap = args[r];
+//             args[r] = args[i];
+//             args[i] = swap;
+//         }
+//         // Generate random integers in range 0 to 999
+//  
